c74fa0be1185c02439c424d06c500a8ec5f741ab
T3Nero <ryan-16@hotmail.com>
2025-11-06 13:11:51 +0000

    Added Game State Logger

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
+from logger import log_state
 
 def main():
     pygame.init()
@@ -25,11 +26,17 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
+        log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         updatable.update(dt)
 
         for a in asteroids:
